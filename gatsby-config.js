7c0ffe702b67b199d039a8992d679f2250e886cb
[Erik Bahena] <[erikjbahena@gmail.com]>
2022-01-15 19:48:32 -0800

    wip, resolve issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,6 @@ module.exports = {
     title: "Roose Paint",
     description:
       "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
-    author: "Erik Bahena",
-    siteURL: `${__dirname}`,
   },
   plugins: [
     "gatsby-plugin-react-helmet",
