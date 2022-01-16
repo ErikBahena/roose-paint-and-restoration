5eb15dc22a7089a112f37ef5bc6f0f694c2cdf5c
[Erik Bahena] <[erikjbahena@gmail.com]>
2022-01-15 20:44:19 -0800

    wip

--- src/pages/gallery/index.js
+++ src/pages/gallery/index.js
@@ -2,5 +2,312 @@ import React from "react";
 import PageTitle from "../../components/PageTitle";
 
 export default function index({ location }) {
-  return <PageTitle location={location} />;
+  return (
+    <>
+      <PageTitle location={location} />
+      <section className="portfolio__area pt-125 pb-100">
+        <div className="container">
+          <div className="row">
+            <div className="col-xxl-6 offset-xxl-3 col-xl-8 offset-xl-2 col-lg-8 offset-lg-2">
+              <div className="section__title-wrapper mb-80 text-center">
+                <span className="section__title-pre section__title-pre-3 center">
+                  Latest Project
+                </span>
+                <h2 className="section__title section__title-2">
+                  Explore Our Professional Interior Projects
+                </h2>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-6">
+              <div className="portfolio__item-3 mb-30 p-relative fix">
+                <div className="portfolio__thumb w-img">
+                  <img src="assets/img/portfolio/03/portfolio-1.jpg" alt />
+                </div>
+                <div className="portfolio__content-3 transition-3 d-flex align-items-center justify-content-between">
+                  <div className="portfolio__content-3-inner">
+                    <h3 className="portfolio__title-3">
+                      <a href="portfolio-details.html">Painting Outside</a>
+                    </h3>
+                    <p>Color Painting Services</p>
+                  </div>
+                  <div className="portfolio__plus-3 transition-3">
+                    <a
+                      href="assets/img/portfolio/03/portfolio-1.jpg"
+                      className="popup-image"
+                    >
+                      <i className="far fa-plus" />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-6">
+              <div className="portfolio__item-3 mb-30 p-relative fix">
+                <div className="portfolio__thumb w-img">
+                  <img src="assets/img/portfolio/03/portfolio-2.jpg" alt />
+                </div>
+                <div className="portfolio__content-3 transition-3 d-flex align-items-center justify-content-between">
+                  <div className="portfolio__content-3-inner">
+                    <h3 className="portfolio__title-3">
+                      <a href="portfolio-details.html">Painting Outside</a>
+                    </h3>
+                    <p>Color Painting Services</p>
+                  </div>
+                  <div className="portfolio__plus-3">
+                    <a
+                      href="assets/img/portfolio/03/portfolio-2.jpg"
+                      className="popup-image"
+                    >
+                      <i className="far fa-plus" />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-6">
+              <div className="portfolio__item-3 mb-30 p-relative fix">
+                <div className="portfolio__thumb w-img">
+                  <img src="assets/img/portfolio/03/portfolio-3.jpg" alt />
+                </div>
+                <div className="portfolio__content-3 transition-3 d-flex align-items-center justify-content-between">
+                  <div className="portfolio__content-3-inner">
+                    <h3 className="portfolio__title-3">
+                      <a href="portfolio-details.html">Painting Outside</a>
+                    </h3>
+                    <p>Color Painting Services</p>
+                  </div>
+                  <div className="portfolio__plus-3 transition-3">
+                    <a
+                      href="assets/img/portfolio/03/portfolio-3.jpg"
+                      className="popup-image"
+                    >
+                      <i className="far fa-plus" />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-6">
+              <div className="portfolio__item-3 mb-30 p-relative fix">
+                <div className="portfolio__thumb w-img">
+                  <img src="assets/img/portfolio/03/portfolio-4.jpg" alt />
+                </div>
+                <div className="portfolio__content-3 transition-3 d-flex align-items-center justify-content-between">
+                  <div className="portfolio__content-3-inner">
+                    <h3 className="portfolio__title-3">
+                      <a href="portfolio-details.html">Painting Outside</a>
+                    </h3>
+                    <p>Color Painting Services</p>
+                  </div>
+                  <div className="portfolio__plus-3 transition-3">
+                    <a
+                      href="assets/img/portfolio/03/portfolio-4.jpg"
+                      className="popup-image"
+                    >
+                      <i className="far fa-plus" />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-6">
+              <div className="portfolio__item-3 mb-30 p-relative fix">
+                <div className="portfolio__thumb w-img">
+                  <img src="assets/img/portfolio/03/portfolio-5.jpg" alt />
+                </div>
+                <div className="portfolio__content-3 transition-3 d-flex align-items-center justify-content-between">
+                  <div className="portfolio__content-3-inner">
+                    <h3 className="portfolio__title-3">
+                      <a href="portfolio-details.html">Painting Outside</a>
+                    </h3>
+                    <p>Color Painting Services</p>
+                  </div>
+                  <div className="portfolio__plus-3 transition-3">
+                    <a
+                      href="assets/img/portfolio/03/portfolio-5.jpg"
+                      className="popup-image"
+                    >
+                      <i className="far fa-plus" />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-6">
+              <div className="portfolio__item-3 mb-30 p-relative fix">
+                <div className="portfolio__thumb w-img">
+                  <img src="assets/img/portfolio/03/portfolio-6.jpg" alt />
+                </div>
+                <div className="portfolio__content-3 transition-3 d-flex align-items-center justify-content-between">
+                  <div className="portfolio__content-3-inner">
+                    <h3 className="portfolio__title-3">
+                      <a href="portfolio-details.html">Painting Outside</a>
+                    </h3>
+                    <p>Color Painting Services</p>
+                  </div>
+                  <div className="portfolio__plus-3 transition-3">
+                    <a
+                      href="assets/img/portfolio/03/portfolio-6.jpg"
+                      className="popup-image"
+                    >
+                      <i className="far fa-plus" />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-6">
+              <div className="portfolio__item-3 mb-30 p-relative fix">
+                <div className="portfolio__thumb w-img">
+                  <img src="assets/img/portfolio/03/portfolio-7.jpg" alt />
+                </div>
+                <div className="portfolio__content-3 transition-3 d-flex align-items-center justify-content-between">
+                  <div className="portfolio__content-3-inner">
+                    <h3 className="portfolio__title-3">
+                      <a href="portfolio-details.html">Painting Outside</a>
+                    </h3>
+                    <p>Color Painting Services</p>
+                  </div>
+                  <div className="portfolio__plus-3 transition-3">
+                    <a
+                      href="assets/img/portfolio/03/portfolio-7.jpg"
+                      className="popup-image"
+                    >
+                      <i className="far fa-plus" />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-6">
+              <div className="portfolio__item-3 mb-30 p-relative fix">
+                <div className="portfolio__thumb w-img">
+                  <img src="assets/img/portfolio/03/portfolio-8.jpg" alt />
+                </div>
+                <div className="portfolio__content-3 transition-3 d-flex align-items-center justify-content-between">
+                  <div className="portfolio__content-3-inner">
+                    <h3 className="portfolio__title-3">
+                      <a href="portfolio-details.html">Painting Outside</a>
+                    </h3>
+                    <p>Color Painting Services</p>
+                  </div>
+                  <div className="portfolio__plus-3 transition-3">
+                    <a
+                      href="assets/img/portfolio/03/portfolio-8.jpg"
+                      className="popup-image"
+                    >
+                      <i className="far fa-plus" />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-6">
+              <div className="portfolio__item-3 mb-30 p-relative fix">
+                <div className="portfolio__thumb w-img">
+                  <img src="assets/img/portfolio/03/portfolio-9.jpg" alt />
+                </div>
+                <div className="portfolio__content-3 transition-3 d-flex align-items-center justify-content-between">
+                  <div className="portfolio__content-3-inner">
+                    <h3 className="portfolio__title-3">
+                      <a href="portfolio-details.html">Painting Outside</a>
+                    </h3>
+                    <p>Color Painting Services</p>
+                  </div>
+                  <div className="portfolio__plus-3 transition-3">
+                    <a
+                      href="assets/img/portfolio/03/portfolio-9.jpg"
+                      className="popup-image"
+                    >
+                      <i className="far fa-plus" />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-6">
+              <div className="portfolio__item-3 mb-30 p-relative fix">
+                <div className="portfolio__thumb w-img">
+                  <img src="assets/img/portfolio/03/portfolio-10.jpg" alt />
+                </div>
+                <div className="portfolio__content-3 transition-3 d-flex align-items-center justify-content-between">
+                  <div className="portfolio__content-3-inner">
+                    <h3 className="portfolio__title-3">
+                      <a href="portfolio-details.html">Painting Outside</a>
+                    </h3>
+                    <p>Color Painting Services</p>
+                  </div>
+                  <div className="portfolio__plus-3 transition-3">
+                    <a
+                      href="assets/img/portfolio/03/portfolio-10.jpg"
+                      className="popup-image"
+                    >
+                      <i className="far fa-plus" />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-6">
+              <div className="portfolio__item-3 mb-30 p-relative fix">
+                <div className="portfolio__thumb w-img">
+                  <img src="assets/img/portfolio/03/portfolio-11.jpg" alt />
+                </div>
+                <div className="portfolio__content-3 transition-3 d-flex align-items-center justify-content-between">
+                  <div className="portfolio__content-3-inner">
+                    <h3 className="portfolio__title-3">
+                      <a href="portfolio-details.html">Painting Outside</a>
+                    </h3>
+                    <p>Color Painting Services</p>
+                  </div>
+                  <div className="portfolio__plus-3 transition-3">
+                    <a
+                      href="assets/img/portfolio/03/portfolio-11.jpg"
+                      className="popup-image"
+                    >
+                      <i className="far fa-plus" />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-6">
+              <div className="portfolio__item-3 mb-30 p-relative fix">
+                <div className="portfolio__thumb w-img">
+                  <img src="assets/img/portfolio/03/portfolio-12.jpg" alt />
+                </div>
+                <div className="portfolio__content-3 transition-3 d-flex align-items-center justify-content-between">
+                  <div className="portfolio__content-3-inner">
+                    <h3 className="portfolio__title-3">
+                      <a href="portfolio-details.html">Painting Outside</a>
+                    </h3>
+                    <p>Color Painting Services</p>
+                  </div>
+                  <div className="portfolio__plus-3 transition-3">
+                    <a
+                      href="assets/img/portfolio/03/portfolio-12.jpg"
+                      className="popup-image"
+                    >
+                      <i className="far fa-plus" />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-xxl-12">
+              <div className="portfolio__more text-center mt-50">
+                <a href="portfolio.html" className="r-btn r-btn-yellow">
+                  view more project <i className="far fa-arrow-right" />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>{" "}
+    </>
+  );
 }
