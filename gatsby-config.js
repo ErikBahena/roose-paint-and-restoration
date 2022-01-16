a45cc9a20d5c70840882c93971fc563492fa413d
[Erik Bahena] <[erikjbahena@gmail.com]>
2022-01-15 22:16:11 -0800

    gallery wip posts cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,8 +26,8 @@ module.exports = {
     {
       resolve: "gatsby-source-filesystem",
       options: {
-        path: `${__dirname}/src/pages`,
-        name: "pages",
+        path: `${__dirname}/content/galleryPosts`,
+        name: "galleryPosts",
       },
     },
     {
---
+++ src/templates/GalleryPost.js
@@ -0,0 +1,53 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { graphql, Link } from "gatsby";
+
+// eslint-disable-next-line
+const GalleryPost = ({ data }) => {
+  return (
+    <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-6">
+      <div className="portfolio__item-3 mb-30 p-relative fix">
+        <div className="portfolio__thumb w-img">
+          <img src="assets/img/portfolio/03/portfolio-1.jpg" />
+        </div>
+        <div className="portfolio__content-3 transition-3 d-flex align-items-center justify-content-between">
+          <div className="portfolio__content-3-inner">
+            <h3 className="portfolio__title-3">
+              <a href="portfolio-details.html">Painting Outside</a>
+            </h3>
+            <p>Color Painting Services</p>
+          </div>
+          <div className="portfolio__plus-3 transition-3">
+            <a
+              href="assets/img/portfolio/03/portfolio-1.jpg"
+              className="popup-image"
+            >
+              <i className="far fa-plus" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+GalleryPost.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.object,
+  }),
+};
+
+export default GalleryPost;
+
+// export const pageQuery = graphql`
+//   query GalleryPostByID($id: String!) {
+//     markdownRemark(id: { eq: $id }) {
+//       id
+//       frontmatter {
+//         date(formatString: "MMMM DD, YYYY")
+//         title
+//         description
+//       }
+//     }
+//   }
+// `;
