2eedfce29a2b5cb0b832f9b443cf604026970a2b
[Erik Bahena] <[erikjbahena@gmail.com]>
2022-01-19 21:45:08 -0800

    wip on contact request form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,19 @@
 import React from "react";
 import PageTitle from "../components/PageTitle";
+import "../assets/css/jobberForm.css";
 
-export default function contact({ location }) {
-  return <PageTitle location={location} />;
+export default function Contact({ location }) {
+  return (
+    <>
+      <PageTitle location={location} />
+
+      <div className="jobber-form-wrapper">
+        <iframe
+          className="jobber-work-request"
+          src="https://clienthub.getjobber.com/client_hubs/66a10fe9-55e4-46ed-b1f4-f244789fa028/public/work_request/embedded_new?source=embedded_inline"
+          sandbox="allow-forms allow-scripts allow-same-origin allow-modals"
+        />
+      </div>
+    </>
+  );
 }
