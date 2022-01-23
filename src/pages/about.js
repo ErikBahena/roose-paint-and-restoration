2ea7ba8b1b19cfebc2c7bd120537a44bc1b1f8ad
[Erik Bahena] <[erikjbahena@gmail.com]>
2022-01-22 21:10:18 -0800

    iss res

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PageTitle from "../components/PageTitle";
-import MainSection from "../components/about/mainSection";
+import MainSection from "../components/about/MainSection";
 import AboutTeam from "../components/about/AboutTeam";
 import AboutWhy from "../components/about/AboutWhy";
 import Testimonials from "../components/Testimonials";
