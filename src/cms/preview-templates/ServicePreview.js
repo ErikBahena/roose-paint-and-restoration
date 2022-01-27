fe91e5d22381d44d12325c1fae3e4f0f56163023
[Erik Bahena] <[erikjbahena@gmail.com]>
2022-01-26 21:55:16 -0800

    wip on cms, naming issue

--- src/cms/preview-templates/ServicePreview.js
+++ src/cms/preview-templates/ServicePreview.js
@@ -2,16 +2,14 @@ import React from "react";
 import HomeService from "../../templates/HomeService";
 
 export default function ServicePreview({ entry, getAsset }) {
-  let bg = getAsset(entry.getIn(["data", "home_image"])).toString();
+  let bg = getAsset(entry.getIn(["data", "homeImage"])).toString();
   let title = entry.getIn(["data", "title"]);
   let description = entry.getIn(["data", "shortDescription"]);
 
   return (
     <>
-      <div style={{ width: "50%" }}>
-        On Home Page
-        <HomeService bg={bg} title={title} description={description} />
-      </div>
+      On Home Page
+      <HomeService bg={bg} title={title} description={description} />
     </>
   );
 }
--- src/templates/HomeService.js
+++ src/templates/HomeService.js
@@ -2,11 +2,18 @@ import React from "react";
 import { Link } from "gatsby";
 
 export default function HomeService(data) {
+  // isLimit checks if this component is being called through the cms preview, if it is since the cms preview won't have gatsbys childImageSharp fluid sizer, we have to do it manually
+  let isLimit = data.bg;
+
   return (
-    <div className="col-xxl-4 col-xl-4 col-lg-6 col-md-6">
+    <div
+      className="col-xxl-4 col-xl-4 col-lg-6 col-md-6"
+      style={isLimit && { maxWidth: "370px" }}
+    >
       <div className="services__item p-relative mb-30">
         <div className="services__thumb w-img">
           <img
+            style={isLimit && { maxHeight: "370px", maxHeight: "370px" }}
             src={
               data.bg || data.frontmatter?.homeImage.childImageSharp.fluid.src
             }
